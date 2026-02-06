73f91ce80c47f5e4bfe732bf2b3b84ffeed3f07f
FaMaLyZ <immerth5@gmail.com>
2026-02-06 10:22:41 +0700

    work in progress
    Finish player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public InputAction moveAction;
+    public InputAction shootAction;
+    public float xRange;
+    public GameObject foodPrefab;
+
     private void Awake()
     {
+        moveAction = InputSystem.actions.FindAction("Move");
 
+        shootAction = InputSystem.actions.FindAction("Shoot");
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //xRange = Camera.main.orthographicSize; 
 
+        var HorizontalInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(HorizontalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (shootAction.triggered)
+        {
+            Instantiate(foodPrefab, transform.position, Quaternion.identity);
+            Debug.Log("ok");
+            Debug.Log(shootAction.triggered);
+        }      
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine
+            (
+                new Vector3(-xRange, transform.position.y, transform.position.z),
+                new Vector3(xRange, transform.position.y, transform.position.z)
+            );
     }
 }
